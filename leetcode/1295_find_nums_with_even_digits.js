1e4798e5c5d1b900ca7a465768400151ac409cb0
Colin Kim <ck2344@nyu.edu>
2020-01-05 01:30:05 -0500

    1295 complete

---
+++ leetcode/1295_find_nums_with_even_digits.js
@@ -0,0 +1,42 @@
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation:
+12 contains 2 digits (even number of digits).
+345 contains 3 digits (odd number of digits).
+2 contains 1 digit (odd number of digits).
+6 contains 1 digit (odd number of digits).
+7896 contains 4 digits (even number of digits).
+Therefore only 12 and 7896 contain an even number of digits.
+Example 2:
+
+Input: nums = [555,901,482,1771]
+Output: 1
+Explanation:
+Only 1771 contains an even number of digits.
+*/
+
+//Solution
+//Time O(n) where n = total number of digits
+//Constant Space
+
+var findNumbers = function(nums) {
+  let count = 0;
+  let digits = 0;
+  nums.forEach(num => {
+    while (num >= 1) {
+      digits++;
+      num = Math.floor(num / 10);
+    }
+    if (digits % 2 === 0) {
+      count++;
+    }
+    digits = 0;
+  });
+  return count;
+};
